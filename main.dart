77dada305c99c18cadb6db1704c6ea08d9079636
2Diere-Bodian2022 <118024178+2Diere-Bodian2022@users.noreply.github.com>
2023-02-10 21:18:25 +0000

    Add files via upload
    page principal modifier

--- main.dart
+++ main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:page_inscription/MesPages/Fromulaire.dart';
-
+import 'MesPages/PageAcceuil/HomePage.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -12,42 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //vrai debut
     return MaterialApp(
-    home:Scaffold(
-       appBar:AppBar(
-         title:const Text("Acceuil"),
-       ),
-      body: Center(
-        child:Column(
-        children:   [
-          Image.asset('Diere/Images/Logo_Dolibar.png',
-          fit: BoxFit.cover,),
-         const Text("Bienvenue sur Dolibar",
-            style: TextStyle(
-              fontSize: 24,
-              fontFamily: 'Itim'
-            ),
-           ),
-        ElevatedButton(
-          onPressed: () {
+    home:HomePage(),
 
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context)=> PageInscription()
-            ),
-              //pageBuilder:(_, __, ___)=> PageInscription()
-          );
-            },
-
-
-            child: Text("Je m'inscris",
-          style: TextStyle(
-            fontSize: 30
-          ) ,
-            ),
-        )
-        ],
-        ),
-      ),
-    ),
     );
   }
 }
+
+
+
+
