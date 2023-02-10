368ae6f89ff64ec45ea08e314fb6f0325452e978
2Diere-Bodian2022 <118024178+2Diere-Bodian2022@users.noreply.github.com>
2023-02-10 21:16:33 +0000

    Add files via upload
    Pages d'acceuill

---
+++ HomePage.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../Page de Connexion/ConnexionPage.dart';
+//Ma page Acceuil
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Acceuil"),
+
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            //Le logo
+            Image.asset('Diere/Images/Logo_Dolibar.png'),
+            const Text("Bienvenue sur Dolibar",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Itim'
+              ),
+            ),
+            //Pour le Bouton
+            ElevatedButton(
+              onPressed:(){
+                Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                        pageBuilder:(_, __, ___) =>PageConnexion()
+                    )
+                );
+              },
+              child: Text("Se Connecter"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
